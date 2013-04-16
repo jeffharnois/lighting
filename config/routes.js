1e16e7f6979336e26c2b77c27ce8d40acba65247
Jeff Harnois <jeffharnois@gmail.com>
2013-04-15 21:16:24 -0700

    adding sprites and adding icons to buttons, fixing twoline, and adding the text (because the client can't let anything go untouched

--- config/routes.js
+++ config/routes.js
@@ -8,7 +8,8 @@ module.exports = function (app, passport, auth, config, hoganTemplateRenderer) {
 
   // user routes
   var users = require(config.root + '/app/controllers/users')
-    , picks = require(config.root+ '/app/controllers/picks');
+    , picks = require(config.root+ '/app/controllers/picks')
+    , features = require(config.root + '/app/models/features.json');
 
   // app.get('/logout', users.logout);
 
@@ -25,49 +26,6 @@ module.exports = function (app, passport, auth, config, hoganTemplateRenderer) {
   //   users.session);
 
   app.post('/createAndSavePicks', users.createAndSavePicks);
-
-  var features = [
-    {
-      feature: "dimming",
-      featureName: "Dimming",
-      pick: [
-        {id: 1, type: "No Dimming", desc: false},
-        {id: 2, type: "0-10 Dimming", desc: "Basic analog dimming"},
-        {id: 3, type: "Phase Cut Triac", desc: "Residential wall mounted dimming"},
-        {id: 4, type: "SMARTY phone dimming", desc: "Remove control wiht SMART phone"}
-      ]
-    },
-    {
-      feature: "sensorandtech",
-      featureName: "Sensor and Technology",
-      pick: [
-        {id: 5, type: "Occupancy Motion Sensor", desc: "Monitor movement and people in area of light"},
-        {id: 6, type: "Environmental Sensor CO2", desc: "Monitor local environmental factors"},
-        {id: 7, type: "Ambient Light Sensor", desc: "Monitor ambient light levels and adjust light output for  constant illumination"},
-        {id: 8, type: "Daylight Harvesting", desc: "Redice lighting when daylight gets brighter to save energy"}
-      ]
-    },
-    {
-      feature: "controlfeatures",
-      featureName: "Control Features",
-      pick: [
-        {id: 9, type: "Diagnostic Health Check", desc: "Light system function self-check"},
-        {id: 10, type: "Color Tuning", desc: "User can adjust light color (CCT)"},
-        {id: 11, type: "Fire Police Emergency Control", desc: "Emergency responders control lighting"},
-        {id: 12, type: "Scheduled Dimming", desc: "Pre-set periods of dimming to save energy"}
-      ]
-    },
-    {
-      feature: "communication",
-      featureName: "Communication",
-      pick: [
-        {id: 13, type: "DALI", desc: "Software configurable broadband configurable radio protocol"},
-        {id: 14, type: "ZigBee", desc: "Mesh wireless based on low power IEEE 80 2wireless"},
-        {id: 15, type: "Zwave", desc: "Wireless mesh networking"},
-        {id: 16, type: "6LoWPAN", desc: "Internet protocol over low power wireless"}
-      ]
-    }
-  ];
 
   app.get("/bracket", function(req, res) {
     res.locals = {
