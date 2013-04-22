cd3b5db7c69c2e70d94fd0049963fd8c6a1f53fb
Jeff Harnois <jeffharnois@gmail.com>
2013-04-22 00:11:25 -0700

    updating

--- config/routes.js
+++ config/routes.js
@@ -9,7 +9,7 @@ module.exports = function (app, passport, auth, config, hoganTemplateRenderer) {
   // user routes
   var users = require(config.root + '/app/controllers/users')
     , picks = require(config.root+ '/app/controllers/picks')
-    , csvParse = require(config.root + '/app/controllers/csv')
+    // , csvParse = require(config.root + '/app/controllers/csv')
     , features = require(config.root + '/app/models/features.json');
 
   // app.get('/logout', users.logout);
@@ -72,58 +72,58 @@ module.exports = function (app, passport, auth, config, hoganTemplateRenderer) {
     });
   });
 
-  var csv = [{
-    "user": "4567",
-    "finalPick": "Daylight Driven Lighting",
-    "bracketFinal": {
-        "firstRound": {
-            "first": "Daylight Driven Lighting",
-            "second": "Fire Police Emergeny Control"
-        }
-    },
-    "bracketFour": {
-        "secondRound": "Daylight Driven Lighting",
-        "firstRound": {
-            "first": "DALI",
-            "second": "ZWave"
-        }
-    },
-    "bracketThree": {
-        "secondRound": "Fire Police Emergeny Control",
-        "firstRound": {
-            "first": "Diagnostic Health Check",
-            "second": "Fire Police Emergeny Control"
-        }
-    },
-    "bracketTwo": {
-        "secondRound": "DALI",
-        "firstRound": {
-            "first": "Occupancy Motion Sensor",
-            "second": "Daylight Driven Lighting"
-        }
-    },
-    "bracketOne": {
-        "secondRound": "Smart Phone Dimming",
-        "firstRound": {
-            "first": "No Dimming",
-            "second": "Smart Phone Dimming"
-        }
-    }
-}];
+//   var csv = [{
+//     "user": "4567",
+//     "finalPick": "Daylight Driven Lighting",
+//     "bracketFinal": {
+//         "firstRound": {
+//             "first": "Daylight Driven Lighting",
+//             "second": "Fire Police Emergeny Control"
+//         }
+//     },
+//     "bracketFour": {
+//         "secondRound": "Daylight Driven Lighting",
+//         "firstRound": {
+//             "first": "DALI",
+//             "second": "ZWave"
+//         }
+//     },
+//     "bracketThree": {
+//         "secondRound": "Fire Police Emergeny Control",
+//         "firstRound": {
+//             "first": "Diagnostic Health Check",
+//             "second": "Fire Police Emergeny Control"
+//         }
+//     },
+//     "bracketTwo": {
+//         "secondRound": "DALI",
+//         "firstRound": {
+//             "first": "Occupancy Motion Sensor",
+//             "second": "Daylight Driven Lighting"
+//         }
+//     },
+//     "bracketOne": {
+//         "secondRound": "Smart Phone Dimming",
+//         "firstRound": {
+//             "first": "No Dimming",
+//             "second": "Smart Phone Dimming"
+//         }
+//     }
+// }];
 
-  app.get('/csv', function(req, res) {
-    csvParse.convert(csv, function(resp) {
-      res.locals = {
-        data: resp
-      };
-      res.render("layout", {
-        partials: {
-          partial: "partials/csv"
-        }
-      });
-    });
+//   app.get('/csv', function(req, res) {
+//     csvParse.convert(csv, function(resp) {
+//       res.locals = {
+//         data: resp
+//       };
+//       res.render("layout", {
+//         partials: {
+//           partial: "partials/csv"
+//         }
+//       });
+//     });
     
-  });
+//   });
 
   app.get("/", function(req, res) {
     res.render("layout", {
